6531a0fa44127d1223996f142f51209cc26e33c9
Suhan Ha <10433434+chemistryx@users.noreply.github.com>
2025-07-19 23:30:09 +0900

    implement go to previous page on not found route

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,14 @@
 "use client"
 import Heading from "@/components/Heading";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function NotFoundPage() {
+    const router = useRouter();
     return (
         <>
             <Heading title="Not Found" description="요청하신 페이지를 찾을 수 없습니다." />
-            <Link href="/">돌아가기</Link>
+            <Link href="" onClick={() => router.back()}>돌아가기</Link>
         </>
     );
 }
