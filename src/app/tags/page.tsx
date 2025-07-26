61aef47a787c9a3675645e9be51788c137dc51a1
Suhan Ha <10433434+chemistryx@users.noreply.github.com>
2025-07-27 03:11:03 +0900

    do not count draft record on tags page

--- src/app/tags/page.tsx
+++ src/app/tags/page.tsx
@@ -6,15 +6,20 @@ export default async function TagsPage() {
     const supabase = await createClient();
     const { data } = await supabase
         .from(TAGS_TABLE_NAME)
-        .select("id, name, slug, records:record_tags(count)")
+        .select("id, name, slug, records:record_tags(record:records(id, draft))")
         .order("created_at");
 
-    const tags = data?.map((d) => ({
-        id: d.id,
-        name: d.name,
-        slug: d.slug,
-        count: d.records[0].count ?? 0,
-    })).filter((tag) => tag.count > 0);
+    const tags = data?.map((d) => {
+        const records = d.records?.flatMap((r) => r.record).filter((record) => record.draft === false);
+
+        return {
+            id: d.id,
+            name: d.name,
+            slug: d.slug,
+            count: records.length ?? 0,
+        };
+    }).filter((tag) => tag.count > 0);
+
 
     return <TagsPageComponent tags={tags ?? []} />
 }
