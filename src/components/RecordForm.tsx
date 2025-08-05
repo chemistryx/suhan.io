8e9ca54d596dc2812bcd7427630a524c68a8eb4e
Suhan Ha <10433434+chemistryx@users.noreply.github.com>
2025-08-06 00:37:34 +0900

    show available tags in select

--- src/components/RecordForm.tsx
+++ src/components/RecordForm.tsx
@@ -6,6 +6,8 @@ import MarkdownEditor from "./MarkdownEditor";
 import { normalize } from "@/utils/strings";
 import CreatableSelect from "react-select/creatable";
 import styles from "@/styles/components/RecordForm.module.scss";
+import { createClient } from "@/utils/supabase/client";
+import { TAGS_TABLE_NAME } from "@/constants";
 
 export type RecordFormData = Pick<Record, "id" | "title" | "slug" | "content" | "draft"> & {
     tags: string[]
@@ -18,11 +20,27 @@ interface Props {
     mode: "create" | "edit";
 }
 
+type SelectOption = { label: string, value: string };
+
 const RecordForm = ({ initialValues, onSubmit, onDirtyChange, mode }: Props) => {
     const defaultValues: RecordFormData = { id: -1, title: "", slug: "", content: "", tags: [], draft: true };
     const [formData, setFormData] = useState({ ...defaultValues, ...initialValues });
-
     const initialRef = useRef({ ...defaultValues, ...initialValues });
+    const [tagOptions, setTagOptions] = useState<SelectOption[]>([]);
+
+    useEffect(() => {
+        const loadTags = async () => {
+            const supabase = createClient();
+
+            const { data, error } = await supabase.from(TAGS_TABLE_NAME).select("name");
+            console.log(data, error);
+            if (data) {
+                setTagOptions(data.map((d) => ({ label: d.name, value: d.name })));
+            }
+        };
+
+        loadTags();
+    }, []);
 
     useEffect(() => {
         const isDirty = formData.title !== initialRef.current.title || formData.content !== initialRef.current.content || formData.draft !== initialRef.current.draft;
@@ -53,6 +71,7 @@ const RecordForm = ({ initialValues, onSubmit, onDirtyChange, mode }: Props) => 
                 placeholder="태그 입력"
                 value={formData.tags.map((tag) => ({ label: tag, value: tag }))}
                 onChange={(value) => setFormData({ ...formData, tags: value.map((tag) => tag.value) })}
+                options={tagOptions}
                 isMulti
             />
             <Input label="초안 (체크 시 비공개)" type="checkbox" checked={formData.draft} onChange={(e) => setFormData({ ...formData, draft: e.target.checked })} />
