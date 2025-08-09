d2c62c781224ca5276ed624bd06f7b20e07b95f8
Suhan Ha <10433434+chemistryx@users.noreply.github.com>
2025-08-09 21:24:19 +0900

    use inline svg images

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,5 @@
 "use client"
 import styles from "@/styles/components/Footer.module.scss";
-import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import SignInModal from "./modals/SignInModal";
@@ -22,12 +21,18 @@ const Footer = () => {
                 <ul className={styles.items}>
                     <li className={styles.item}>
                         <Link className={styles.link} href="https://github.com/chemistryx" target="_blank">
-                            <Image src="/icons/github.svg" width={20} height={20} alt="github" />
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-github-icon lucide-github">
+                                <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+                                <path d="M9 18c-4.51 2-5-2-7-2" />
+                            </svg>
                         </Link>
                     </li>
                     <li className={styles.item}>
                         <Link className={styles.link} href="mailto:starpiung@naver.com">
-                            <Image src="/icons/mail.svg" width={20} height={20} alt="mail" />
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-mail-icon lucide-mail">
+                                <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" />
+                                <rect x="2" y="4" width="20" height="16" rx="2" />
+                            </svg>
                         </Link>
                     </li>
                 </ul>
