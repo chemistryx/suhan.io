04832a116f01005e755d51f930ff7b5562a4f7ad
Suhan Ha <10433434+chemistryx@users.noreply.github.com>
2025-08-10 02:25:34 +0900

    add gap between elements on 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,17 +2,18 @@
 import Button from "@/components/Button";
 import { Heading, HeadingDescription, HeadingTitle } from "@/components/Heading";
 import { useRouter } from "next/navigation";
+import styles from "@/styles/pages/NotFoundPage.module.scss";
 
 export default function NotFoundPage() {
     const router = useRouter();
 
     return (
-        <>
+        <div className={styles.base}>
             <Heading>
                 <HeadingTitle>Not Found</HeadingTitle>
                 <HeadingDescription>요청하신 페이지를 찾을 수 없습니다.</HeadingDescription>
             </Heading>
             <Button onClick={() => router.back()}>돌아가기</Button>
-        </>
+        </div>
     );
 }
