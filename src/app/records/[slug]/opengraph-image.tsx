de30aa2fb670d3a754c8be7f13ceadc2bf5986d8
Suhan Ha <10433434+chemistryx@users.noreply.github.com>
2025-08-28 03:19:38 +0900

    make sure og image generation runs on nodejs runtime
    * due to edge function size limitation (1MB)

--- src/app/records/[slug]/opengraph-image.tsx
+++ src/app/records/[slug]/opengraph-image.tsx
@@ -7,6 +7,7 @@ import { join } from "path";
 
 export const size = { width: 1200, height: 630 };
 export const contentType = "image/png";
+export const runtime = "nodejs";
 
 interface Props {
     children: React.ReactNode;
